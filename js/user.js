55dc219bd8e03dcdb8497b2f4f716f79bffe977d
maripiero23 <mari.pieropan@gmail.com>
2022-10-26 16:10:49 +0200

    modifiche: parentesi e virgole

--- js/user.js
+++ js/user.js
@@ -1,26 +1,26 @@
 //chiedo all'utente come si chiama
-const userName = prompt("Come ti chiami?")
+const userName = prompt("Come ti chiami?");
 
 //chiedo all'utente il suo cognome
-const user2Name = prompt("Qual'è il tuo cognome?")
+const userCognome = prompt("Qual'è il tuo cognome?");
 
 
 //chiedo all'utente il suo colore preferito
-const userColor = prompt("Qual'è il tuo colore preferito?")
+const userColor = prompt("Qual'è il tuo colore preferito?");
 
 //salvo in una variabile un riferimento all'elemento html con id
-const h2CompleteUser = document.getElementById("info_utente")
+const h2CompleteUser = document.getElementById("info_utente");
 
 //uso il riferimento precedente
-h2CompleteUser.innerHTML = (userName + user2Name + userColor + 21)
+h2CompleteUser.innerHTML = userName + userCognome + userColor + 21;
 
 
 
 
 
 // FACENDO DELLE PROVE
-let name2NameColor=userName + " " +user2Name +userColor
+let nameCognomeColor=userName + " " +userCognome +userColor
 
-console.log("Total info=", name2NameColor)
+console.log("Total info=", nameCognomeColor)
 
-console.log(`L'utente si chiama + ${userName} + e di cognome fa + ${user2Name}+ mentre il suo colore preferito è il +${userColor}`)
+console.log(`L'utente si chiama + ${userName} + e di cognome fa + ${userCognome}+ mentre il suo colore preferito è il +${userColor}`)
