2cf905a30d12c44e363f9b87b13823584cc0bcf5
maripiero23 <mari.pieropan@gmail.com>
2022-10-26 15:13:23 +0200

    finito

---
+++ js/user.js
@@ -0,0 +1,15 @@
+//chiedo all'utente come si chiama
+const userName = prompt("Come ti chiami?")
+
+//chiedo all'utente il suo cognome
+const user2Name = prompt("Qual'è il tuo cognome?")
+
+
+//chiedo all'utente il suo colore preferito
+const userColor = prompt("Qual'è il tuo colore preferito?")
+
+//salvo in una variabile un riferimento all'elemento html con id
+const h2CompleteUser = document.getElementById("info_utente")
+
+//uso il riferimento precedente
+h2CompleteUser.innerHTML = (userName + user2Name + userColor + 21)
