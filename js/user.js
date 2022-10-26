351adb2a88b9700596d5bc27204d6b2b860b6f85
maripiero23 <mari.pieropan@gmail.com>
2022-10-26 15:45:35 +0200

    fine2

--- js/user.js
+++ js/user.js
@@ -13,3 +13,12 @@ const h2CompleteUser = document.getElementById("info_utente")
 
 //uso il riferimento precedente
 h2CompleteUser.innerHTML = (userName + user2Name + userColor + 21)
+
+
+
+
+
+// FACENDO DELLE PROVE
+let name2NameColor=userName + " " +user2Name +userColor
+
+console.log("Total info=", name2NameColor)
